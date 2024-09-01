e9959f51ba08642e4ba6281e09913ad242f78e42
Eduard Puchaew <jamool@gmx.de>
2024-09-01 11:45:24 +0200

    first commit

---
+++ models/world.class.js
@@ -0,0 +1,57 @@
+class World {
+  character = new Character();
+  enemies = level1.enemies;
+  clouds = level1.clouds;
+  backgroundObjects = level1.backgroundObjects;
+  canvas;
+  ctx;
+  keyboard;
+  camera_x = 0;
+
+  constructor(canvas, keyboard) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.keyboard = keyboard;
+    this.draw();
+    this.setWorld();
+  }
+
+  setWorld() {
+    this.character.world = this;
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.heigth);
+    this.ctx.translate(this.camera_x, 0);
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.clouds);
+    this.addObjectsToMap(this.enemies);
+    this.ctx.translate(-this.camera_x, 0);
+
+    self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(mo) {
+    if (mo.otherDirection) {
+      this.ctx.save();
+      this.ctx.translate(mo.width, 0);
+      this.ctx.scale(-1, 1);
+      mo.x = mo.x * -1;
+    }
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.heigth);
+    if (mo.otherDirection) {
+        mo.x = mo.x * -1;
+      this.ctx.restore();
+    }
+  }
+}
