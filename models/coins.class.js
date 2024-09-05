d7155e664f8921dde6332f4d17fc4586fd2d8489
Eduard Puchaew <jamool@gmx.de>
2024-09-05 11:31:29 +0200

    add coin in world

---
+++ models/coins.class.js
@@ -0,0 +1,26 @@
+class Coins extends MovableObject{
+
+    height = 200;
+    width = 200;
+    y = 55
+    
+
+    IMAGES_COINS = [
+        'img/8_coin/coin_1.png',
+        'img/8_coin/coin_2.png'
+      
+    ];
+    constructor(){
+        super().loadImage(this.IMAGES_COINS[0]);
+        this.loadImages(this.IMAGES_COINS);
+        this.x = 200;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_COINS);
+        }, 200);
+    }
+}
+
